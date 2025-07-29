be86f3bc9d70fac6f6509e875f6c25d05e428924
Ryan Rodriguez <ryancode2321@gmail.com>
2025-07-29 16:10:47 -0500

    ball change

--- src/app/page.js
+++ src/app/page.js
@@ -136,18 +136,18 @@ export default function Home() {
                     </p>
 
                     {/* Bouncing Dots (extra if searching) */}
-                    <div className="absolute top-1/4 left-1/4 animate-bounce">
-                      <div className="w-4 h-4 bg-blue-500 rounded-full shadow-lg shadow-blue-500/50"></div>
-                    </div>
-                    <div className="absolute top-1/3 right-1/3 animate-bounce delay-300">
-                      <div className="w-4 h-4 bg-sky-500 rounded-full shadow-lg shadow-sky-500/50"></div>
-                    </div>
-                    <div className="absolute bottom-1/4 left-1/2 animate-bounce delay-700">
-                      <div className="w-4 h-4 bg-indigo-500 rounded-full shadow-lg shadow-indigo-500/50"></div>
-                    </div>
 
                     {status === 'searching' && (
                       <>
+                        <div className="absolute top-1/4 left-1/4 animate-bounce">
+                          <div className="w-4 h-4 bg-blue-500 rounded-full shadow-lg shadow-blue-500/50"></div>
+                        </div>
+                        <div className="absolute top-1/3 right-1/3 animate-bounce delay-300">
+                          <div className="w-4 h-4 bg-sky-500 rounded-full shadow-lg shadow-sky-500/50"></div>
+                        </div>
+                        <div className="absolute top-1/8 right-1/6 animate-bounce delay-700">
+                          <div className="w-4 h-4 bg-indigo-500 rounded-full shadow-lg shadow-indigo-500/50"></div>
+                        </div>
                         <div className="absolute top-2/3 left-1/3 animate-bounce delay-200">
                           <div className="w-4 h-4 bg-pink-400 rounded-full shadow-lg shadow-pink-400/50"></div>
                         </div>
@@ -193,11 +193,11 @@ export default function Home() {
                 />
               ))}
           </div>
-          {!loading && prompt && destinations.length === 0 && (
+          {/* {!loading && prompt && destinations.length === 0 && (
             <p className="text-center text-2xl text-gray-500 mt-10">
               There are no destinations for this location at the moment.
             </p>
-          )}
+          )} */}
         </div>
       </section>
     </div>
